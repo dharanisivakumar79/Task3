b54265b80ea2e61d54813c568707575ea57df515
dharanisivakumar79 <dharanisivakumar79@gmail.com>
2025-10-09 16:45:35 +0530

    Add files via upload

---
+++ Day4.py
@@ -0,0 +1,23 @@
+# Task1
+for i in range(1,10,1):
+	print(i)
+	
+#Task2
+for i in range(1,11):
+	print(i,"× 6 =",i*6)
+	
+#Task3	
+n=int(input("Enter the table number: "))
+for i in range(1,11):
+	print(i,"×",n,"=",i*n)
+	
+#Task4
+list=[]
+n=int(input("Enter the table number: "))
+for i in range(1,11):
+	c=i*n
+	list.append(c)
+print(list)
+
+	
+	
