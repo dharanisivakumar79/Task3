8da3d4e215d125ad112764f0224da21caa614fc8
dharanisivakumar79 <dharanisivakumar79@gmail.com>
2025-10-10 17:04:17 +0530

    Add files via upload
    Day5 task

---
+++ Day5.py
@@ -0,0 +1,26 @@
+# Task1
+for i in range(10,0,-1):
+    print(i)
+
+# Task2
+n=0
+while n<5:
+    n+=1
+    print("Dharani")
+
+# Task3
+n=0
+while n<=10:
+    n+=1
+    print(n)
+    if n==5:
+        break
+
+# Task4
+n=int(input("Enter the number: "))
+fact=1
+for i in range(1,n+1):
+    fact=fact*i
+print(fact)
+
+
